02527c7ebc8f971f97e5a4719648ab81fcd56390
bigfriggnrig <rpmusic@outlook.com.au>
2026-09-18 10:36:41 +1000

    Student Number Update

--- startHere.m
+++ startHere.m
@@ -4,9 +4,9 @@
 % Modify the below variables to contain the student numbers of you and your
 % group members. If you have less than three members, use 12345678 for the
 % remaining student numbers.
-sid1 = 12345678;
-sid2 = 12345678;
-sid3 = 12345678;
+sid1 = 11739606;
+sid2 = 12319244;
+sid3 = 10722017;
 
 % Now, run this script and ensure the DataA2.mat file has been generated.
 
